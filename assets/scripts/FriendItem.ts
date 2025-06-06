c6d189b03bd61327a14ccc84ad99fb1c63ba3daf
harry <harry0966199411@gmail.com>
2025-06-07 04:23:55 +0800

    c

--- assets/scripts/FriendItem.ts
+++ assets/scripts/FriendItem.ts
@@ -80,9 +80,10 @@ export class FriendItem extends Component {
             this.moneyLabel.string = `$${friendData.money || 0}`;
         }
         
-        if (this.statusLabel) {
+        /*if (this.statusLabel) {
             this.statusLabel.string = hasStolen ? 'Stolen' : '';
-        }
+        }*/
+       this.setupFriendStatus(hasStolen);
         
         // 如果是待處理的朋友請求，將訪問按鈕更改為接受按鈕
         if (isPending && this.visitButton) {
